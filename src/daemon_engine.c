195314be1eab53cc2c2fc52d169012ad45faffe3
Mohamed Teka <tekamed88@gmail.com>
2012-08-03 12:24:08 +0100

    implement getCMD

--- src/daemon_engine.c
+++ src/daemon_engine.c
@@ -1,1 +1,16 @@
 #include "daemon_engine.h"
+
+char *getCMD()  {
+
+CURL * session = curl_easy_init();
+char url="http://localhost/remoteCMD/server.php?getcmd=1";
+
+if(session){
+    curl_easy_setopt(session,CURLOPT_URL,url);
+    curl_easy_perform(session);
+    curl_easy_cleanup(session);
+    }else
+        printf("erreur lors de l'initialisation de la fonction curl_easy_init()\n"); 
+ 
+
+}
