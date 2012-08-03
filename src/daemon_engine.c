7e27f558c76d1a3269defae6662dfac4ffc9d3d5
Zaafouri Amine <zaafouri.amine@gmail.com>
2012-08-03 13:09:46 +0100

    indentation du fichier daemon_engine.c

--- src/daemon_engine.c
+++ src/daemon_engine.c
@@ -1,16 +1,15 @@
 #include "daemon_engine.h"
 
-char *getCMD()  {
+char *getCMD(){
 
-CURL * session = curl_easy_init();
-char url="http://localhost/remoteCMD/server.php?getcmd=1";
+	CURL * session = curl_easy_init();
+	char url="http://localhost/remoteCMD/server.php?getcmd=1";
 
-if(session){
-    curl_easy_setopt(session,CURLOPT_URL,url);
-    curl_easy_perform(session);
-    curl_easy_cleanup(session);
-    }else
-        printf("erreur lors de l'initialisation de la fonction curl_easy_init()\n"); 
- 
-return " cmd received " ;
+	if(session){
+    		curl_easy_setopt(session,CURLOPT_URL,url);
+    		curl_easy_perform(session);
+    		curl_easy_cleanup(session);
+    	}else
+        printf("erreur lors de l\'initialisation de la fonction curl_easy_init()\n"); 
+	return "cmd received";
 }
